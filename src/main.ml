8058a0673c3954b898f86002acc76e8b1afea87d
sebamiro <seba.mi.21@gmail.com>
2024-02-08 14:38:03 +0100

    Starting Turing machine

--- src/main.ml
+++ src/main.ml
@@ -6,6 +6,35 @@
 
 type action = Left | Right
 
+type state = string
+
+type transition = {
+    read: char;
+    write: char;
+    to_state: state;
+    action: action;
+}
+
+type machine = {
+    alphabe: string;
+    blank: char;
+    states: state list;
+    finals: state list;
+}
+
+let print cur tape =
+    String.iteri (fun i c ->
+        if i = cur then Printf.printf "[%c]" c
+        else
+        Printf.printf "%c" c
+    ) tape;
+    print_endline ""
+
 let () =
-    raise (Failure "Sorry")
+    let tape = "Hello, World!" in
+    let rec loop cur tape =
+        if cur < String.length tape then
+            print cur tape;
+            loop (cur + 1) tape
+    in loop 0 tape
 
