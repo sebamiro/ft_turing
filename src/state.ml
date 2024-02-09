c97b4453cf87cdd53ddcc214ab2d48119d678c01
sebamiro <seba.mi.21@gmail.com>
2024-02-09 19:26:40 +0100

    Fixed compare function

--- src/state.ml
+++ src/state.ml
@@ -6,7 +6,12 @@
 
 module T = struct
     type t = string * char
-        let compare (s1, c1) (s2, c2) = String.compare s1 s2
+        let compare (s1, c1) (s2, c2) =
+            let x = String.compare s1 s2 in
+            if x > 0 then 1
+            else if x < 0 then -1
+            else
+            Char.compare c1 c2
 end
 
 include T
